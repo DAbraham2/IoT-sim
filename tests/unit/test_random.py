326b20ade738fc612ed81e6ebe802bd3c4ad5d30
DAbraham2 <daniel.abraham@edu.bme.hu>
2023-02-02 10:18:58 +0100

    basic project structure

---
+++ tests/unit/test_random.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class Test(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(1, 1)
+
+    def test_something_else(self):
+        self.assertEqual(2, 2)
